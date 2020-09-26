78df23c0caadbc0e3c1c8a6c1a24f3daef9ff002
heterodain <mamaroyal.alpha@gmail.com>
2020-09-27 04:39:20 +0900

    長期運用対応

--- src/main/java/com/heterodain/smartmeter/App.java
+++ src/main/java/com/heterodain/smartmeter/App.java
@@ -1,212 +1,103 @@
 package com.heterodain.smartmeter;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-import com.fazecast.jSerialComm.SerialPort;
+import com.heterodain.smartmeter.device.SmartMeter;
+import com.heterodain.smartmeter.device.SmartMeter.Power;
+import com.heterodain.smartmeter.service.Ambient;
 
+import lombok.var;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Hello world!
- *
+ * スマートメーター監視
  */
 @Slf4j
-public class App 
-{
-    private static final int AMBIENT_CHANNEL = 99999;
-    private static final String AMBIENT_WRITE_KEY = "****************";
+public class App {
+    private static final String COM_PORT = "ttyUSB0";
 
-    private static final String BROUTE_ID = "********************************";
+    private static final int AMBIENT_CHANNEL = 999999;
+    private static final String AMBIENT_WRITE_KEY = "************";
+
+    private static final String BROUTE_ID = "*********************************";
     private static final String BROUTE_PASSWORD = "************";
 
-    private static final String SKVER_COMMAND = "SKVER\r\n";
-    private static final String SKSETPWD_COMMAND = "SKSETPWD C %s\r\n";
-    private static final String SKSETRBID_COMMAND = "SKSETRBID %s\r\n";
-    private static final String SKSCAN_COMMAND = "SKSCAN 2 FFFFFFFF %X\r\n";
-    private static final String SKLL64_COMMAND = "SKLL64 %s\r\n";
-    private static final String SKSREG_COMMAND = "SKSREG %s %s\r\n";
-    private static final String SKJOIN_COMMAND = "SKJOIN %s\r\n";
-    private static final String SKSENDTO_COMMAND = "SKSENDTO 1 %s 0E1A 1 %04x ";
-    private static final byte[] ECHONET_LITE_FRAME = { 0x10, (byte) 0x81, 0x00, 0x01, 0x05, (byte) 0xFF, 0x01, 0x02, (byte) 0x88, 0x01, 0x62, 0x02, (byte) 0xE7, 0x00, (byte) 0xEA, 0x00 };
+    private static ScheduledExecutorService threadPool = Executors.newScheduledThreadPool(2);
 
-    public static void main( final String[] args ) throws Exception {
-        SerialPort serial = SerialPort.getCommPort("ttyUSB0"); // COM3
-        serial.setBaudRate(115200);
-        serial.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 3000, 3000);
-        if (!serial.openPort()) {
-            throw new IOException("シリアルポートが開けませんでした。");
+    public static void main(final String[] args) throws Exception {
+        var ambient = new Ambient(AMBIENT_CHANNEL, "TODO", AMBIENT_WRITE_KEY);
+
+        try (var smartMeter = new SmartMeter(COM_PORT, BROUTE_ID, BROUTE_PASSWORD)) {
+            smartMeter.init();
+            smartMeter.connect();
+
+            var powers = new ArrayList<Power>();
+
+            // 10秒毎にスマートメーターから電力情報読込
+            Runnable readSmartMeterTask = () -> {
+                try {
+                    var power = smartMeter.getCurrentPower();
+                    synchronized (powers) {
+                        powers.add(power);
+                    }
+
+                } catch (InterruptedException e) {
+                    // NOP
+                } catch (Exception e) {
+                    log.warn("スマートメーターへのアクセスに失敗しました。", e);
+                }
+            };
+            threadPool.scheduleWithFixedDelay(readSmartMeterTask, 0, 10, TimeUnit.SECONDS);
+
+            // 1分毎にAmbientにデータ送信
+            Runnable sendAmbientTask = () -> {
+                try {
+                    Double rw, tw;
+                    synchronized (powers) {
+                        // R相の1分間平均電力(W)算出
+                        rw = powers.stream().mapToDouble(p -> {
+                            double w = p.getInstantPower();
+                            long r = p.getInstantRAmp();
+                            long t = p.getInstantTAmp();
+                            long a = r + t;
+                            return a == 0 ? 0D : w * r / a;
+                        }).average().orElse(0D);
+
+                        // T相の1分間平均電力(W)算出
+                        tw = powers.stream().mapToDouble(p -> {
+                            double w = p.getInstantPower();
+                            long r = p.getInstantRAmp();
+                            long t = p.getInstantTAmp();
+                            long a = r + t;
+                            return a == 0 ? 0D : w * t / a;
+                        }).average().orElse(0D);
+
+                        powers.clear();
+                    }
+                    ambient.send(rw, tw);
+
+                } catch (Exception e) {
+                    log.warn("Ambientへのデータ送信に失敗しました。", e);
+                }
+            };
+            threadPool.scheduleWithFixedDelay(sendAmbientTask, 1, 1, TimeUnit.MINUTES);
+
+            // SIGINT(Ctrl + C)で止められるまで待つ
+            var wait = new Object();
+            synchronized (wait) {
+                try {
+                    wait.wait();
+                } catch (InterruptedException e) {
+                    // NOP
+                }
+            }
+
+            threadPool.shutdown();
+            threadPool.awaitTermination(15, TimeUnit.SECONDS);
         }
-
-        try (BufferedReader r = new BufferedReader(new InputStreamReader(serial.getInputStream(), StandardCharsets.ISO_8859_1))) {
-            // バージョン番号取得
-            writeCommand(serial, SKVER_COMMAND);
-            readLine(r);
-            readLine(r);
-            if (!"OK".equals(readLine(r))) {
-                throw new IOException("SKVERコマンドが失敗しました。");
-            }
-
-            // Bルートパスワード設定
-            writeCommand(serial, String.format(SKSETPWD_COMMAND, BROUTE_PASSWORD));
-            readLine(r);
-            if (!"OK".equals(readLine(r))) {
-                throw new IOException("SKSETPWDコマンドが失敗しました。");
-            }
-
-            // BルートID設定
-            writeCommand(serial, String.format(SKSETRBID_COMMAND, BROUTE_ID));
-            readLine(r);
-            if (!"OK".equals(readLine(r))) {
-                throw new IOException("SKSETRBIDコマンドが失敗しました。");
-            }
-
-            // スマートメーター探索
-            serial.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 20000, 20000);
-
-            Map<String, String> smInfo = new HashMap<>();
-            writeCommand(serial, String.format(SKSCAN_COMMAND, 6));
-            while (true) {
-                String line = readLine(r);
-                if (line.startsWith("EVENT 22")) {
-                    break;       
-                } else if (line.startsWith("  ")) {
-                    String[] result = line.trim().split(":");
-                    smInfo.put(result[0], result[1]);
-                }
-            }
-            if (!smInfo.containsKey("Channel")) {
-                throw new IOException("スマートメーターが見つかりませんでした。");
-            }
-            log.info("スマートメーターが見つかりました。{}", smInfo);
-
-            serial.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 3000, 3000);
-
-            // アドレス変換(MAC→IPV6)
-            writeCommand(serial, String.format(SKLL64_COMMAND, smInfo.get("Addr")));
-            readLine(r);
-            String address = readLine(r).trim();
-
-            // R7023に通信先スマートメータのチャンネルを設定
-            writeCommand(serial, String.format(SKSREG_COMMAND, "S2", smInfo.get("Channel")));
-            readLine(r);
-            if (!"OK".equals(readLine(r))) {
-                throw new IOException("SKSREGコマンドが失敗しました。");
-            }
-
-            // R7023に通信先スマートメータのPan IDを設定
-            writeCommand(serial, String.format(SKSREG_COMMAND, "S3", smInfo.get("Pan ID")));
-            readLine(r);
-            if (!"OK".equals(readLine(r))) {
-                throw new IOException("SKSREGコマンドが失敗しました。");
-            }
-
-            // スマートメータに接続
-            serial.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 10000, 10000);
-            writeCommand(serial, String.format(SKJOIN_COMMAND, address));
-            readLine(r);
-            if (!"OK".equals(readLine(r))) {
-                throw new IOException("SKJOINコマンドが失敗しました。");
-            }
-
-            while (true) {
-                String line = readLine(r);
-                if (line.startsWith("EVENT 24")) {
-                    throw new IOException("スマートメーター接続に失敗しました。");
-                } else if (line.startsWith("EVENT 25")) {
-                    break;
-                }
-            }
-
-            serial.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 3000, 3000);
-
-            while (true) {
-                writeEchonetLite(serial, String.format(SKSENDTO_COMMAND, address, ECHONET_LITE_FRAME.length), ECHONET_LITE_FRAME);
-
-                Long power = null;
-                Double power30 = null;
-                do {
-                    String line = readLine(r);
-                    if (line.startsWith("ERXUDP")) {
-                        String res = line.trim().split(" ")[8];
-                        String seoj = res.substring(8, 8 + 6);
-                        String esv = res.substring(20, 20 + 2);
-                        if ("028801".equals(seoj) && "72".equals(esv)) {
-                            int pos = 24;
-                            while (pos < res.length()) {
-                                String epc = res.substring(pos, pos + 2);
-                                pos += 2;
-                                int epcSize = Integer.parseInt(res.substring(pos, pos + 2), 16);
-                                pos += 2;
-                                String epcData = res.substring(pos, pos + epcSize * 2);
-                                pos += epcSize * 2;
-
-                                if ("E7".equals(epc)) {
-                                    power = Long.parseLong(epcData, 16);
-                                    log.info("瞬時電力(W): {}", power);
-                                } else if ("EA".equals(epc)) {
-                                    power30 = ((double) Long.parseLong(epcData.substring(14), 16)) / 10D;
-                                    log.info("30分積算電力(kWh): {}", power30);
-                                }
-                            }
-                        }
-                    }
-                } while (r.ready());
-
-                if (power != null && power30 != null) {
-                    // Ambientに送信
-                    URL url = new URL("http://54.65.206.59/api/v2/channels/" + AMBIENT_CHANNEL + "/dataarray");
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Content-Type", "application/json");
-                    conn.setConnectTimeout(10000);
-                    conn.setReadTimeout(10000);
-                    conn.setDoOutput(true);
-
-                    String json = String.format("{\"writeKey\":\"%s\",\"data\":[{\"d1\":%d,\"d2\":%f}]}", AMBIENT_WRITE_KEY, power, power30);
-                    log.debug(json);
-
-                    try {
-                        try (OutputStream os = conn.getOutputStream()) {
-                            os.write(json.getBytes());
-                        }
-                        conn.getResponseCode();
-
-                    } catch (Exception e) {
-                        log.error("Ambientデータ送信に失敗しました。", e);
-                    }
-                }
-
-                Thread.sleep(60000);
-            }
-       }
-    }
-
-    private static String readLine(BufferedReader r) throws IOException {
-        String line = r.readLine();
-        log.debug("Recieve: {}", line);
-        return line;
-    }
-
-    private static void writeCommand(SerialPort serial, String command) throws IOException {
-        log.debug("Send: {}", command);
-        byte[] bytes = command.getBytes();
-        serial.writeBytes(bytes, bytes.length);
-    }
-
-    private static void writeEchonetLite(SerialPort serial, String command, byte[] frame) throws IOException {
-        log.debug("Send: {}{}", command, Arrays.toString(frame));
-        byte[] head = command.getBytes();
-        byte[] bytes = ByteBuffer.allocate(head.length + frame.length).put(head).put(frame).array();
-        serial.writeBytes(bytes, bytes.length);
     }
 }
---
+++ src/main/java/com/heterodain/smartmeter/service/Ambient.java
@@ -0,0 +1,187 @@
+package com.heterodain.smartmeter.service;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.Data;
+import lombok.var;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Ambient {
+    // JSONマッパー
+    private static ObjectMapper om = new ObjectMapper();
+
+    // チャネルID
+    private int channelId;
+    // READキー
+    private String readKey;
+    // WRITEキー
+    private String writeKey;
+
+    /**
+     * コンストラクタ
+     * 
+     * @param channelId チャンネルID
+     * @param readKey   READキー
+     * @param writeKey  WRITEキー
+     */
+    public Ambient(int channelId, String readKey, String writeKey) {
+        this.channelId = channelId;
+        this.readKey = readKey;
+        this.writeKey = writeKey;
+    }
+
+    /**
+     * チャネルにデータ送信
+     * 
+     * @param datas 送信データ(最大8個)
+     * @throws IOException
+     */
+    public void send(Double... datas) throws IOException {
+        // 送信するJSONを構築
+        var rootNode = om.createObjectNode();
+        rootNode.put("writeKey", this.writeKey);
+
+        var dataArrayNode = om.createArrayNode();
+        var dataNode = om.createObjectNode();
+        for (int i = 1; i <= datas.length; i++) {
+            if (datas[i - 1] != null) {
+                dataNode.put("d" + i, datas[i - 1]);
+            }
+        }
+        dataArrayNode.add(dataNode);
+        rootNode.set("data", dataArrayNode);
+
+        var jsonString = om.writeValueAsString(rootNode);
+
+        // HTTP POST
+        var url = "http://54.65.206.59/api/v2/channels/" + channelId + "/dataarray";
+        log.debug("request > " + url);
+        log.debug("body > " + jsonString);
+
+        var conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setConnectTimeout(10000);
+        conn.setReadTimeout(10000);
+        conn.setDoOutput(true);
+        try (var os = conn.getOutputStream()) {
+            os.write(jsonString.getBytes(StandardCharsets.UTF_8));
+        }
+        int resCode = conn.getResponseCode();
+        if (resCode != 200) {
+            throw new IOException("Ambient Response Code " + resCode);
+        }
+    }
+
+    /**
+     * 1日分のデータ取得
+     * 
+     * @param date 日付
+     * @return 1日分のデータ
+     * @throws IOException
+     */
+    public JsonNode read(LocalDate date) throws IOException {
+        // HTTP GET
+        var url = "http://54.65.206.59/api/v2/channels/" + channelId + "/data?readKey=" + readKey + "&date="
+                + date.format(DateTimeFormatter.ISO_DATE);
+        log.debug("request > " + url);
+
+        var conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setRequestMethod("GET");
+        conn.setConnectTimeout(10000);
+        conn.setReadTimeout(20000);
+        var resCode = conn.getResponseCode();
+        if (resCode != 200) {
+            throw new IOException("Ambient Response Code " + resCode);
+        }
+
+        try (var is = conn.getInputStream()) {
+            return om.readTree(is);
+        }
+    }
+
+    /**
+     * 指定期間のデータ取得
+     * 
+     * @param start 開始日
+     * @param end   終了日
+     * @return 指定期間のデータ
+     * @throws IOException
+     */
+    public List<ReadData> read(LocalDateTime start, LocalDateTime end) throws IOException {
+        // HTTP GET
+        var url = "http://54.65.206.59/api/v2/channels/" + channelId + "/data?readKey=" + readKey;
+        url += "&start=" + URLEncoder.encode(start.format(DateTimeFormatter.ISO_DATE_TIME), "UTF-8");
+        url += "&end=" + URLEncoder.encode(end.format(DateTimeFormatter.ISO_DATE_TIME), "UTF-8");
+        log.debug("request > " + url);
+
+        var conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setRequestMethod("GET");
+        conn.setConnectTimeout(10000);
+        conn.setReadTimeout(20000);
+        var resCode = conn.getResponseCode();
+        if (resCode != 200) {
+            throw new IOException("Ambient Response Code " + resCode);
+        }
+
+        try (var is = conn.getInputStream()) {
+            return om.readValue(is, new TypeReference<List<ReadData>>() {
+            });
+        }
+    }
+
+    /**
+     * 直近n個のデータ取得
+     * 
+     * @param n 取得個数
+     * @return 直近n個のデータ
+     * @throws IOException
+     */
+    public List<ReadData> read(int n) throws IOException {
+        // HTTP GET
+        var url = "http://54.65.206.59/api/v2/channels/" + channelId + "/data?readKey=" + readKey + "&n=" + n;
+        log.debug("request > " + url);
+
+        var conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setRequestMethod("GET");
+        conn.setConnectTimeout(10000);
+        conn.setReadTimeout(20000);
+        var resCode = conn.getResponseCode();
+        if (resCode != 200) {
+            throw new IOException("Ambient Response Code " + resCode);
+        }
+
+        try (var is = conn.getInputStream()) {
+            return om.readValue(is, new TypeReference<List<ReadData>>() {
+            });
+        }
+    }
+
+    @Data
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class ReadData {
+        private String created;
+        private Double d1;
+        private Double d2;
+        private Double d3;
+        private Double d4;
+        private Double d5;
+        private Double d6;
+        private Double d7;
+        private Double d8;
+    }
+}
